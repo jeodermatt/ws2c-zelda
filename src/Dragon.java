2ddb9c95f26e2f2ffdeb822653f4b17ff08aae82
Vithushan Mahendran <vithushan.mahendran@students.fhnw.ch>
2017-04-24 16:53:52 +0200

    Dragon Object erstellt und Plan.png

--- src/Dragon.java
+++ src/Dragon.java
@@ -4,4 +4,23 @@
 import processing.core.PApplet;
 
 public class Dragon extends PApplet{
+    public static void main(String[] args){
+        PApplet.main("Dragon", args);
+    }
+
+    public void settings(){
+        size(600,400);
+    }
+
+    public void setup(){
+
+        background(255);
+    }
+
+    public void draw(){
+        stroke(0);
+        fill(255,0,0);
+        ellipse(width/2, height/2, 100, 100);
+    }
+
 }
